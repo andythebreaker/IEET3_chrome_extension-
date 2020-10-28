585c71714f7c49d5db6c52d8283ace3d7ae2de17
andythebreaker <43373581+andythebreaker@users.noreply.github.com>
2020-10-28 16:43:14 +0800

    Add files via upload

---
+++ IEET3/autoclick.js
@@ -0,0 +1,6 @@
+document.querySelectorAll("input[value=\"3\"]").forEach(el => {
+    el.click();
+});
+
+document.querySelectorAll("input[name=\"ans[y6]\"]")[0].click();
+document.querySelectorAll("button[type=\"submit\"]")[0].click();
